246a9fa822faff3f8151df51b71487a56f9a9d60
Dhananjay-JSR <ajaysenday@gmail.com>
2024-09-02 02:03:43 +0530

    websock irl

--- client/src/utils/conn.ts
+++ client/src/utils/conn.ts
@@ -1,2 +1,3 @@
+export const BTC_ws = new WebSocket("wss://snowden.wtf/btc")
 export const ETH_ws = new WebSocket("wss://snowden.wtf/eth")
 export const NewReports_ws = new WebSocket("wss://snowden.wtf/liveReports")
